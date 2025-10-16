236309adaac3d2cc369a89da4731e491ffd3753c
Muhammad Muazzain <90068510+MuhammadMuazzain@users.noreply.github.com>
2025-10-17 01:09:36 +0500

    Done1

--- chatapp.py
+++ chatapp.py
@@ -6,9 +6,9 @@ from langchain_google_genai import GoogleGenerativeAIEmbeddings
 import google.generativeai as genai
 from langchain.vectorstores import FAISS
 from langchain_google_genai import ChatGoogleGenerativeAI
-from langchain.chains.question_answering import load_qa_chain
-from langchain.prompts import PromptTemplate
-from dotenv import load_dotenv
+# from langchain.chains.question_answering import load_qa_chain
+# from langchain.prompts import PromptTemplate
+# from dotenv import load_dotenv
 
 load_dotenv()
 # os.getenv("GOOGLE_API_KEY")
